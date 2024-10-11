90f7fe945604023a65f0032968745082f1116ca2
Mexic1 <cmorar100@yahoo.ro>
2024-10-11 22:57:52 +0300

    ex1

--- src/ex1/EX1.java
+++ src/ex1/EX1.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class EX1 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
 
-
+    }
 }
